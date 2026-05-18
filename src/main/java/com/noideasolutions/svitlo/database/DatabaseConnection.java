b4afd1c13883db705db81827c806157de8e95be6
Yevhenii Kovalenko <zhenya.kovalenko322@gmail.com>
2026-05-18 19:28:27 +0300

    Прибрано пароль від бази даних з відкритого доступу

--- src/main/java/com/noideasolutions/svitlo/database/DatabaseConnection.java
+++ src/main/java/com/noideasolutions/svitlo/database/DatabaseConnection.java
@@ -1,20 +1,36 @@
 package com.noideasolutions.svitlo.database;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://ep-lucky-rain-al41w67r-pooler.c-3.eu-central-1.aws.neon.tech/neondb?user=neondb_owner&password=npg_YfjuXKas10rM&sslmode=require&channelBinding=require";
-
     private static Connection connection = null;
+
+    // Метод, який читає файл db.properties
+    private static String getDbUrl() {
+        Properties props = new Properties();
+        try (FileInputStream in = new FileInputStream("db.properties")) {
+            props.load(in);
+            return props.getProperty("db.url");
+        } catch (IOException e) {
+            System.err.println("Не знайдено файл db.properties! Створіть його в корені проєкту.");
+            return null;
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
         if (connection == null || connection.isClosed()) {
+            String url = getDbUrl();
+            if (url == null) throw new SQLException("URL бази даних порожній.");
+
             try {
                 Class.forName("org.postgresql.Driver");
-                connection = DriverManager.getConnection(URL);
-                System.out.println("Успішно підключено до Neon PostgreSQL!");
+                connection = DriverManager.getConnection(url);
+                System.out.println("Успішно підключено до Neon PostgreSQL (безпечний режим)!");
             } catch (ClassNotFoundException e) {
                 System.err.println("Драйвер PostgreSQL не знайдено!");
                 e.printStackTrace();
