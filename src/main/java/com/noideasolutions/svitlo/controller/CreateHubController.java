dbb9cc4278e4f12b60281801ab2f3f6d8327ef47
Yevhenii Kovalenko <zhenya.kovalenko322@gmail.com>
2026-06-11 13:51:53 +0300

    Підключені вікна 2

--- src/main/java/com/noideasolutions/svitlo/controller/CreateHubController.java
+++ src/main/java/com/noideasolutions/svitlo/controller/CreateHubController.java
@@ -1,6 +1,9 @@
 package com.noideasolutions.svitlo.controller;
 
 import com.noideasolutions.svitlo.model.Hub;
+import com.noideasolutions.svitlo.service.HubService;
+import com.noideasolutions.svitlo.service.UserSession;
+import com.noideasolutions.svitlo.util.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -24,6 +27,8 @@ public class CreateHubController {
     @FXML
     private TextField slotsField;
 
+    private HubService hubService = new HubService();
+
     @FXML
     private void handleAddHub(ActionEvent event) {
         String title = titleField.getText();
@@ -41,14 +46,24 @@ public class CreateHubController {
             double latitude = Double.parseDouble(latStr);
             double longitude = Double.parseDouble(lonStr);
             int slots = Integer.parseInt(slotsStr);
-            int currentHostId = 1;
+
+            // Беремо реальний ID користувача (хоста), який зараз залогінений
+            int currentHostId = UserSession.getInstance().getCurrentUser().getId();
 
             Hub newHub = new Hub(currentHostId, title, description, latitude, longitude, slots);
 
+            hubService.createHub(title, description, latitude, longitude, slots);
+
             showAlert(Alert.AlertType.INFORMATION, "Успіх", "Хаб успішно створено: " + newHub.getTitle());
+
+            // Автоматично повертаємо користувача на Головний Дашборд, щоб він побачив свій новий хаб
+            SceneSwitcher.switchTo(event, "/com/noideasolutions/svitlo/controller/MainDashboard.fxml", "Головне меню");
 
         } catch (NumberFormatException e) {
             showAlert(Alert.AlertType.ERROR, "Помилка формату", "Перевірте правильність вводу координат та кількості місць.");
+        } catch (Exception e) {
+            // Відловлюємо помилки бази даних (наприклад, якщо впаде з'єднання з Neon)
+            showAlert(Alert.AlertType.ERROR, "Помилка БД", "Не вдалося зберегти хаб: " + e.getMessage());
         }
     }
 
